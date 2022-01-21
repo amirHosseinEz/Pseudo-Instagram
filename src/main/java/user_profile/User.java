242a2311992650b3771dca773b6f926398a0aa00
amirHosseinEz <4mirezzati@gmail.com>
2022-01-22 00:48:45 +0330

    User Class

--- src/main/java/user_profile/User.java
+++ src/main/java/user_profile/User.java
@@ -1,28 +1,39 @@
-package user_profile;
+package main.java.user_profile;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import main.java.main.Main;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
+@Table(name = "user")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "username")
     private String username;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "email")
     private String email;
 
     private User() {
     }
 
-    private User(String username, String password) {
+    //////////
+    public User(String username, String password, String email) {
         this.username = username;
         this.password = password;
+        this.email = email;
     }
 
     public static User create(){
@@ -30,11 +41,30 @@ public class User {
         return user;
     }
 
-    public static User create(String username, String password){
-        User user = new User(username, password);
+    public static User create(String username, String password, String email){
+        User user = new User(username, password, email);
         return user;
     }
+    public static User createtest(User user){
+        Session session = Main.factory.getCurrentSession();
+        Transaction transaction = null;
+        try {
+            session.beginTransaction();
+            Query query1 = session.createQuery("from User where username = \'"+user.username+"\' or email =\'"+ user.email + "\'");
+            List<User> collection1 = query1.getResultList();
+            if(!collection1.isEmpty()) {
+                System.out.println("username or email are already taken!");
+                return null;
+            }
 
+            session.save(user);
+            session.getTransaction().commit();;
+        }
+        finally {
+            session.close();
+        }
+        return user;
+    }
     public Long getId() {
         return id;
     }
