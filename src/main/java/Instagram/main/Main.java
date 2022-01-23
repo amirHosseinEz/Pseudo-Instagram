f8a258d8eb869fe5af3b116e63e96b61492c3f03
Reza Hesami <rezahesami10@gmail.com>
2022-01-23 03:35:09 +0330

    javafx works

--- src/main/java/Instagram/main/Main.java
+++ src/main/java/Instagram/main/Main.java
@@ -1,12 +1,17 @@
 package Instagram.main;
 
-import Instagram.user.User;
-import Instagram.user.UserProfile;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Main {
-
+public class Main extends Application {
     public static void main(String[] args) {
-
+        launch(args);
     }
-
+    public static Stage window;
+    @Override
+    public void start(Stage stage) throws Exception {
+        window = stage;
+        window.setTitle("Instagram");
+        window.show();
+    }
 }
