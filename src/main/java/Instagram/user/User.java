74113a4cc80a9b29db2bcc3435a707451b0f4180
amirHosseinEz <4mirezzati@gmail.com>
2022-01-23 12:23:36 +0330

    User Updating functions(saving updates in database)

--- src/main/java/Instagram/user/User.java
+++ src/main/java/Instagram/user/User.java
@@ -186,6 +186,7 @@ public class User {
         return email;
     }
 
+
     public void setEmail(String email) {
         this.email = email;
         Session session =HibernateUtil.getSessionFactory().openSession();
